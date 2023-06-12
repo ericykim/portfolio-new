8278d819eb041b1e089f78e6d2ab6703ff876e45
Eric Kim <ericykim5399@gmail.com>
2023-06-11 22:38:30 -0400

    fix font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,16 +4,28 @@ import ReactDOM from 'react-dom'
 import { Container } from '@/layout'
 import localFont from 'next/font/local';
 import styles from './homePage.module.scss'
+import { classes } from '@/utils';
 
 export const metadata = {
     title: 'Eric Kim',
     description: 'An awesome portfolio',
 }
-const myFont = localFont({ src: './PPAgrandir-Variable.ttf' });
+const myFont = localFont({
+    src: '/PPAgrandir-Variable.ttf',
+ });
+const myFontVariable = localFont({
+    src: '/PPAgrandir-Variable.ttf',
+    variable: '--font-family'
+ });
+
+// --font-family: 'PPAgrandir-light', Helvetica, Arial, sans-serif;
+//     --font-family: 'PPAgrandir-regular', Helvetica, Arial, sans-serif;
+//     --font-family: 'PPAgrandir-bold', Helvetica, Arial, sans-serif;
+//     --font-family:
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
     return (
-        <html lang='en' className={myFont.className}>
+        <html lang='en' className={classes(myFontVariable.variable, myFont.className)}>
             <body>
                 {/* @ts-expect-error Async Server Component */}
                 <Navigation />
